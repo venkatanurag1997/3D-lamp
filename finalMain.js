c061bbbd45e3c7702773a9a5f13d46c9ad03c071
Venkat Anurag <93969402+AnthraX1997@users.noreply.github.com>
2022-12-14 23:22:55 -0500

    Add files via upload

---
+++ finalMain.js
@@ -0,0 +1,477 @@
+  'use strict';
+
+  // Global variables that are set and used
+  // across the application
+  let gl;
+  var myCube = null;
+  var myCylinder = null;
+
+  // GLSL programs
+  let drawProgram;
+  
+  // VAOs for the objects
+
+  // textures
+  let modelTexture;
+
+  // rotation
+
+  var anglesReset = [30.0, 30.0, 0.0];
+  var cube_angles = [30.0, 30.0, 0.0];
+  var sphere_angles = [180.0, 180.0, 0.0];
+  var angles = sphere_angles;
+  var angleInc = 5.0;
+ 
+//
+// create shapes and VAOs for objects.
+// Note that you will need to bindVAO separately for each object / program based
+// upon the vertex attributes found in each program
+//
+function createShapes() {
+  myCube = new Cube(5);
+  myCube.VAO = bindVAO(myCube, drawProgram);
+  myCylinder = new Cylinder (9, 10);
+  myCylinder.VAO = bindVAO(myCylinder, drawProgram);
+
+}
+
+
+//
+// Here you set up your camera position, orientation, and projection
+// Remember that your projection and view matrices are sent to the vertex shader
+// as uniforms, using whatever name you supply in the shaders
+//
+function setUpCamera(program) {
+    
+    gl.useProgram (program);
+
+   //   set up your projection
+  //   defualt is orthographic projection
+  let projMatrix = glMatrix.mat4.create(); 
+  //glMatrix.mat4.ortho(projMatrix,-5,5,-5,5,1.0,300.0)
+  glMatrix.mat4.perspective(projMatrix,radians(70),1,0,null);     
+  gl.uniformMatrix4fv (program.uProjT, false, projMatrix);  
+  // set up your view
+  // defaut is at (0,0,-5) looking at the origin
+  let viewMatrix = glMatrix.mat4.create();
+  glMatrix.mat4.lookAt(viewMatrix, [0,0, -8], [0,0, 0], [0, 1, 0]);
+  gl.uniformMatrix4fv (program.uViewT, false, viewMatrix);
+    
+    // set up your projection
+    
+    // set up your view
+
+}
+
+
+//
+// load up the textures you will use in the shader(s)
+// The setup for the globe texture is done for you
+// Any additional images that you include will need to
+// set up as well.
+//
+
+
+function setUpTextures(){
+    
+  // get some texture space from the gpu
+  modelTexture = gl.createTexture();
+  
+  // load the actual image
+  const worldImage = document.getElementById("world-texture");
+  worldImage.src = 'abc.jpg';
+
+  worldImage.onload = () => {
+      doLoad (modelTexture, worldImage);
+  };
+
+  // marbTexture = gl.createTexture();
+  // const marbImage = document.getElementById("marb-texture");
+  // marbImage.src = 'marble.jpg';
+  // marbImage.onload = () => {
+  //     doLoad (marbTexture,marbImage);
+  // };
+  
+}
+
+function doLoad(theTexture, theImage) {
+  gl.bindTexture(gl.TEXTURE_2D, theTexture);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, theImage);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.bindTexture(gl.TEXTURE_2D, null);
+
+  draw();
+}
+
+
+
+// function setUpPhong(program, a, b, c) {
+//   gl.useProgram(program);
+//   let ambientLights = gl.getUniformLocation(program, "ambientLight");
+//   gl.uniform3fv(ambientLights, [a,a,a]);
+//   let lightColors = gl.getUniformLocation(program, "lightColor");
+//   gl.uniform3fv(lightColors, [b,b,b]);
+//   let lightPositions = gl.getUniformLocation(program, "lightPosition");
+//   gl.uniform3fv(lightPositions, [11, 10, 5]);
+//   let baseColors = gl.getUniformLocation(program, "baseColor");
+//   gl.uniform3fv(baseColors, [a, b, c]);
+//   let specHighlightColors = gl.getUniformLocation(program, "specHighlightColor");
+//   gl.uniform3fv(specHighlightColors, [1.0, 1.0, 1.0]);
+//   let ka = gl.getUniformLocation(program, "ka");
+//   gl.uniform1f(ka, 1);
+//   let ks = gl.getUniformLocation(program, "ks");
+//   gl.uniform1f(ks, 0.1);
+//   let kd = gl.getUniformLocation(program, "kd");
+//   gl.uniform1f(kd, 0.5);
+//   let ke = gl.getUniformLocation(program, "ke");
+//   gl.uniform1f(ke, 1);
+// }
+
+//
+//  This function draws all of the shapes required for your scene
+//
+function drawShapes(program) {
+
+  gl.useProgram(program);
+
+  // let cubeMatrix = glMatrix.mat4.create();
+  // glMatrix.mat4.scale(cubeMatrix, cubeMatrix, [2, 0.5, 3]);
+  // glMatrix.mat4.translate(cubeMatrix, cubeMatrix, [0, -5, 0]);
+  // // gl.uniformMatrix4fv(program.phonguModelT, false, cubeMatrix);
+  // // setUpPhong(program, 1.0, 0.2, 0.6);
+  // gl.activeTexture (gl.TEXTURE0+1);
+  // gl.bindTexture (gl.TEXTURE_2D, modelTexture);
+
+  // gl.bindVertexArray(myCube.VAO);
+  // gl.drawElements(gl.TRIANGLES, myCube.indices.length, gl.UNSIGNED_SHORT, 0);
+  // console.log("Cube here", myCube);
+
+
+  let cylinderMatrix = glMatrix.mat4.create();
+  glMatrix.mat4.scale(cylinderMatrix, cylinderMatrix, [1, 0.5, 1]);
+  // glMatrix.mat4.translate(cylinderMatrix, cylinderMatrix, [1, 5, 5]);
+  //gl.uniformMatrix4fv(program.aVertexPosition, false, cylinderMatrix);
+  gl.uniform1i(program.uTextureValue,1);
+  
+  gl.uniform3fv (program.uTheta, new Float32Array(angles));
+  gl.activeTexture (gl.TEXTURE0);
+  gl.bindTexture (gl.TEXTURE_2D, modelTexture);
+  // gl.uniform1i (program.uAbstractTexture, 1);
+  gl.uniformMatrix4fv(program.uModelT, false, cylinderMatrix);
+
+  gl.bindVertexArray(myCylinder.VAO);
+  gl.drawElements(gl.TRIANGLES, myCylinder.indices.length, gl.UNSIGNED_SHORT, 0);
+  console.log("cylinder: ", myCylinder);
+
+  // gl.useProgram(program);
+  // let coneMatrix = glMatrix.mat4.create();
+  // glMatrix.mat4.scale(coneMatrix, coneMatrix, [2, 0.5, 3]);
+
+  // glMatrix.mat4.translate(coneMatrix, coneMatrix, [-2, -5, 0]);
+  // gl.uniformMatrix4fv(program.phonguModelT, false, coneMatrix);
+  // setUpPhong(program, 1.0, 0.2, 0.6);
+  // gl.bindVertexArray(myCone.VAO);
+  // gl.drawElements(gl.TRIANGLES, myCone.indices.length, gl.UNSIGNED_SHORT, 0);
+  // console.log("Cube here", myCone);
+
+  // gl.useProgram(program);
+  // let cubeMatrix2 = glMatrix.mat4.create();
+  // glMatrix.mat4.scale(cubeMatrix2, cubeMatrix2, [2, 0.5, 3]);
+
+  // glMatrix.mat4.translate(cubeMatrix2, cubeMatrix2, [2, -5, 0]);
+  // gl.uniformMatrix4fv(program.phonguModelT, false, cubeMatrix2);
+  // setUpPhong(program, 1.0, 0.2, 0.6);
+  // gl.bindVertexArray(myCube.VAO);
+  // gl.drawElements(gl.TRIANGLES, myCube.indices.length, gl.UNSIGNED_SHORT, 0);
+  // console.log("Cube here", myCube);
+
+}
+
+  //
+  // Use this function to create all the programs that you need
+  // You can make use of the auxillary function initProgram
+  // which takes the name of a vertex shader and fragment shader
+  //
+  // Note that after successfully obtaining a program using the initProgram
+  // function, you will beed to assign locations of attribute and unifirm variable
+  // based on the in variables to the shaders.   This will vary from program
+  // to program.
+  //
+  function initPrograms () {
+    // set up the per-vertex program
+    // const phongPerVertexShader = getShader('phong-per-vertex-V');
+    // const phongPerFragmentShader = getShader('phong-per-fragment-F');
+    const VertexShaderTxt = getShader('sphereMap-V');
+    const FragmentShaderTxt = getShader('sphereMap-F');
+
+    // Create a program
+    let program = gl.createProgram();
+    
+    // Attach the shaders to this program
+    // gl.attachShader(program, phongPerVertexShader);
+    // gl.attachShader(program, phongPerFragmentShader);
+    gl.attachShader(program, VertexShaderTxt);
+    gl.attachShader(program, FragmentShaderTxt);
+    gl.linkProgram(program);
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+      console.error('Could not initialize shaders');
+    }
+
+    // Use this program instance
+    gl.useProgram(program);
+    // We attach the location of these shader values to the program instance
+    // for easy access later in the code
+    // program.aVertexPosition = gl.getAttribLocation(program, 'aVertexPosition');
+    // program.aNormal = gl.getAttribLocation(program, 'aNormal');
+    // program.phongAVert = gl.getAttribLocation(program,"phongAVertexPosition");
+    // program.sphereAVert = gl.getAttribLocation(program,"sphereAVertexPosition");
+    // program.phonguModelT = gl.getUniformLocation(program, "phongmodelT");
+    // program.phonguViewT = gl.getUniformLocation(program, "phongviewT");
+    // program.phonguProjT = gl.getUniformLocation(program, "phongprojT");
+    // program.sphereuModelT = gl.getUniformLocation(program, "spheremodelT");
+    // program.sphereViewT = gl.getUniformLocation(program, "sphereviewT");
+    // program.sphereProjT = gl.getUniformLocation(program, "sphereprojT");
+    // program.aNormal = gl.getAttribLocation(program, "aNormal");
+     program.uModelT = gl.getUniformLocation (program, 'modelt');
+    // program.uViewT = gl.getUniformLocation (program, 'viewT');
+    // program.uProjT = gl.getUniformLocation (program, 'projT');
+    // program.ambientLight = gl.getUniformLocation (program, 'ambientLight');
+    // program.lightPosition = gl.getUniformLocation (program, 'lightPosition');
+    // program.lightColor = gl.getUniformLocation (program, 'lightColor');
+    // program.baseColor = gl.getUniformLocation (program, 'baseColor');
+    // program.specHighlightColor = gl.getUniformLocation (program, 'specHighlightColor');
+    // program.ka = gl.getUniformLocation (program, 'ka');
+    // program.kd = gl.getUniformLocation (program, 'kd');
+    // program.ks = gl.getUniformLocation (program, 'ks');
+    // program.ke = gl.getUniformLocation (program, 'ke');
+    // program.aUV = gl.getAttribLocation(program, "aUV");
+    // program.uTheTexture = gl.getUniformLocation(program, "theTexture");
+    // program.uCreatedTexture = gl.getUniformLocation(program, "createdTexture");
+    // program.uTxValue = gl.getUniformLocation(program, "txValue");
+      
+
+
+  program.aVertexPosition = gl.getAttribLocation(program, 'aVertexPosition');
+  program.aUV = gl.getAttribLocation(program, 'aUV');
+    
+  // uniforms - you will need to add references for any additional
+  // uniforms that you add to your shaders
+  program.uTheTexture = gl.getUniformLocation (program, 'theTexture');
+  // program.uMarbTexture = gl.getUniformLocation(program, 'marbTexture');
+  program.uTheta = gl.getUniformLocation (program, 'theta');
+  program.background = gl.getUniformLocation(program, 'background');
+  
+  return program;
+  }
+
+  //shader VAO
+function bindVAO(shape, program) {
+  console.log("Binding Shader VAO", shape, "\n now program", program);
+
+  //create and bind VAO
+    let theVAO = gl.createVertexArray();
+    gl.bindVertexArray(theVAO);
+    
+    // create, bind, and fill buffer for vertex locations
+    // vertex locations can be obtained from the points member of the
+    // shape object.  3 floating point values (x,y,z) per vertex are
+    // stored in this array.
+    let myVertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, myVertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(shape.points), gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(program.aVertexPosition);
+    gl.vertexAttribPointer(program.aVertexPosition, 3, gl.FLOAT, false, 0, 0);
+    
+    // create, bind, and fill buffer for uv's
+    // uvs can be obtained from the uv member of the
+    // shape object.  2 floating point values (u,v) per vertex are
+    // stored in this array.
+    let uvBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(shape.uv), gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(program.aUV);
+    gl.vertexAttribPointer(program.aUV, 2, gl.FLOAT, false, 0, 0);
+    
+    // Setting up element array
+    // element indicies can be obtained from the indicies member of the
+    // shape object.  3 values per triangle are stored in this
+    // array.
+    let myIndexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, myIndexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(shape.indices), gl.STATIC_DRAW);
+
+    // Do cleanup
+    gl.bindVertexArray(null);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+    
+    return theVAO;
+}
+
+  // creates a VAO and returns its ID
+  function bindTextureVAO(shape, program) {
+    //create and bind VAO
+    let theVAO = gl.createVertexArray();
+    gl.bindVertexArray(theVAO);
+
+    let myVertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, myVertexBuffer);
+    gl.bufferData( gl.ARRAY_BUFFER,new Float32Array(shape.points), gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(program.sphereAVert);
+    gl.vertexAttribPointer(program.sphereAVert,3, gl.FLOAT, false, 0, 0);
+
+    let uvBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(shape.uv), gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(program.aUV);
+    gl.vertexAttribPointer(program.aUV, 2, gl.FLOAT, false, 0, 0);
+  
+    let myIndexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, myIndexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,new Uint16Array(shape.indices),gl.STATIC_DRAW);
+  
+    // Do cleanup
+    gl.bindVertexArray(null);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+  
+    return theVAO;
+  }
+
+
+/////////////////////////////////////////////////////////////////////////////
+//
+//  You shouldn't have to edit anything below this line...but you can
+//  if you find the need
+//
+/////////////////////////////////////////////////////////////////////////////
+
+// Given an id, extract the content's of a shader script
+// from the DOM and return the compiled shader
+function getShader(id) {
+  const script = document.getElementById(id);
+  const shaderString = script.text.trim();
+
+  // Assign shader depending on the type of shader
+  let shader;
+  if (script.type === 'x-shader/x-vertex') {
+    shader = gl.createShader(gl.VERTEX_SHADER);
+  }
+  else if (script.type === 'x-shader/x-fragment') {
+    shader = gl.createShader(gl.FRAGMENT_SHADER);
+  }
+  else {
+    return null;
+  }
+
+  // Compile the shader using the supplied shader code
+  gl.shaderSource(shader, shaderString);
+  gl.compileShader(shader);
+
+  // Ensure the shader is valid
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    console.error(gl.getShaderInfoLog(shader));
+    return null;
+  }
+
+  return shader;
+}
+
+
+  //
+  // compiles, loads, links and returns a program (vertex/fragment shader pair)
+  //
+  // takes in the id of the vertex and fragment shaders (as given in the HTML file)
+  // and returns a program object.
+  //
+  // will return null if something went wrong
+  //
+  function initProgram(vertex_id, fragment_id) {
+    const vertexShader = getShader(vertex_id);
+    const fragmentShader = getShader(fragment_id);
+
+    // Create a program
+    let program = gl.createProgram();
+      
+    // Attach the shaders to this program
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+      console.error('Could not initialize shaders');
+      return null;
+    }
+      
+    return program;
+  }
+
+
+  //
+  // We call draw to render to our canvas
+  //
+  function draw() {
+    // Clear the scene
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+      
+    // draw your shapes
+    drawShapes(drawProgram);
+
+    // Clean
+    gl.bindVertexArray(null);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+  }
+
+  // Entry point to our application
+  function init() {
+      
+    // Retrieve the canvas
+    const canvas = document.getElementById('webgl-canvas');
+    if (!canvas) {
+      console.error(`There is no canvas with id ${'webgl-canvas'} on this page.`);
+      return null;
+    }
+
+    // deal with keypress
+    window.addEventListener('keydown', gotKey ,false);
+
+    // Retrieve a WebGL context
+    gl = canvas.getContext('webgl2');
+    if (!gl) {
+        console.error(`There is no WebGL 2.0 context`);
+        return null;
+      }
+      
+    // deal with keypress
+    window.addEventListener('keydown', gotKey ,false);
+      
+    // Set the clear color to be black
+    gl.clearColor(0, 0, 0, 1);
+      
+    // some GL initialization
+    gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.CULL_FACE);
+    
+    gl.cullFace(gl.BACK);
+    gl.frontFace(gl.CCW);
+    gl.clearColor(0.0,0.0,0.0,1.0)
+    gl.depthFunc(gl.LEQUAL)
+    gl.clearDepth(1.0)
+
+    // Read, compile, and link your shaders
+    drawProgram = initPrograms();
+
+  
+    
+    // create and bind your current object
+    createShapes();
+    setUpTextures();
+    
+    // do a draw
+    draw();
+  }
